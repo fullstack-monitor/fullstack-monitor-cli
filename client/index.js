5ca02cf228b66a11ba0d81637d46f3804abfe11a
tommyrharper <thomasrobertharper@gmail.com>
2021-03-26 02:24:56 +0000

    Fix theme provider error in index.js

--- client/index.js
+++ client/index.js
@@ -2,7 +2,6 @@ import { ThemeProvider } from '@chakra-ui/core';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
-import {ThemeProvider} from "@chakra-ui/core";
 
 // uncomment so that webpack can bundle styles
 /* eslint-disable no-unused-vars */
@@ -10,7 +9,7 @@ import './scss/application.scss';
 
 render(
   <ThemeProvider>
-  <App />
+    <App />
   </ThemeProvider>,
   document.getElementById('root'),
 );
