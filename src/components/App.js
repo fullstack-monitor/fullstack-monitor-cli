25c4c4e994a1a94b735afb67c4b66a0b9a94f991
tommyrharper <thomasrobertharper@gmail.com>
2021-03-27 22:10:40 +0000

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,42 @@
-import React from "react";
+/* eslint-disable */
+import React, { Component } from "react";
 import "../index.css";
 import { Button } from "@chakra-ui/react";
 import { io } from "socket.io-client";
 
-const socket = io();
+// const socket = io();
 
-const sendWSMessage = () => {
-  socket.emit("chat message", "hi from client");
-  socket.on("chat message", (msg) => {
-    console.log("recieved message from server: ", msg);
-  });
-};
+// {transports: ['websocket']}
 
-const App = ({ props }) => (
-  <div>
-    <Button onClick={sendWSMessage}>Yo</Button>
-    Hello World
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      clientLogs: [],
+      serverLogs: [],
+      requests: [],
+      socket: io()
+    }
+  }
+  componentDidMount() {
+    this.state.socket.on("chat message", (msg) => {
+      console.log("recieved message from server: ", msg);
+    });
+  }
+
+  sendWSMessageArrow = () => {
+    console.log('inside sendWSMessage Arrow');
+    this.state.socket.emit("chat message", "hi from client arrow");
+  };
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.sendWSMessageArrow}>Yo Arrow</Button>
+        Hello World
+      </div>
+    );
+  }
+}
 
 export default App;
