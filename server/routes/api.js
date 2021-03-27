9ba868e93a81c703d53a0399a5e1a92db2aaef85
aye_work1 <amoe.aye@gmail.com>
2021-03-27 15:06:31 -0700

    request/response added to get and post route

--- server/routes/api.js
+++ server/routes/api.js
@@ -8,8 +8,16 @@ router.get('/logs',
   loggerController.getLogs,
   (req, res) => res.status(200).json(res.locals.logs));
 
+router.get('/:type',
+  loggerController.getSingleRequest,
+  (req, res) => res.status(200).json(res.locals.logs));
+
 router.post('/logs/:type',
   loggerController.addLogs,
   (req, res) => res.status(200).json(`Added ${req.params.type} logs`));
+
+router.post('/requests',
+  loggerController.addRequests,
+  (req, res) => res.status(200).json('Requests added!'));
 
 module.exports = router;
