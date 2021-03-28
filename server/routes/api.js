433557e81aad2beab8c68e44af11a16a9c005cc3
tommyrharper <thomasrobertharper@gmail.com>
2021-03-28 00:23:36 +0000

    Add webSocketMiddleware to api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,13 +1,14 @@
 const express = require('express');
-// const { io } = require('../index');
+const { io } = require('../../config');
 
 const loggerController = require('../controllers/loggerController');
 
 const router = express.Router();
 
-// const webSocketMiddleware = () => {
-//   io.emit('chat message', 'sending databack');
-// };
+const webSocketMiddleware = (req, res, next) => {
+  io.emit('chat message', res.locals.logs);
+  next();
+};
 
 router.get('/logs',
   loggerController.getLogs,
@@ -15,10 +16,14 @@ router.get('/logs',
 
 router.post('/logs/:type',
   loggerController.addLogs,
+  loggerController.getLogs,
+  webSocketMiddleware,
   (req, res) => res.status(200).json(`Added ${req.params.type} logs`));
 
 router.post('/requests',
   loggerController.addRequests,
+  loggerController.getLogs,
+  webSocketMiddleware,
   (req, res) => res.status(200).json('Requests added!'));
 
 module.exports = router;
