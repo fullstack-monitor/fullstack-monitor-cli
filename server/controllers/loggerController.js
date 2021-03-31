e03158adba8359512d4777517dd5dc7be2bf5ba6
tommyrharper <thomasrobertharper@gmail.com>
2021-03-31 04:39:09 +0100

    Remove old fetch routes

--- server/controllers/loggerController.js
+++ server/controllers/loggerController.js
@@ -6,33 +6,6 @@ let data;
 
 const loggerController = {};
 
-// middleware to get all type of logs
-loggerController.getLogs = async (req, res, next) => {
-  // read all logs from saved files
-  const logs = await getAllLogs();
-
-  if (!logs) {
-    return next({
-      log: 'loggerController.getLogs:  ERROR: Error getting logs data from allLogs.json file',
-      message: { err: 'Error occurred in loggerController.getLogs. Check server logs for more details.' },
-    });
-  }
-
-  // assign logs into res.locals.logs
-  res.locals.logs = { allLogs: logs };
-
-  return next();
-};
-
-// middleware to add client and server console log to files
-loggerController.addLogs = (req, res, next) => {
-  // assign incoming req.body to logs variable
-  const logs = req.body;
-
-  storeLogs(logs);
-  return next();
-};
-
 // middleware to delete json files
 loggerController.deleteLogs = (req, res, next) => {
   fs.writeFileSync(path.resolve(__dirname, '../data/allLogs.json'), JSON.stringify([]), 'utf8');
--- server/routes/api.js
+++ server/routes/api.js
@@ -5,24 +5,6 @@ const loggerController = require('../controllers/loggerController');
 
 const router = express.Router();
 
-const webSocketMiddleware = (req, res, next) => {
-  io.emit('chat message', res.locals.logs);
-  next();
-};
-
-// route to retrieve all logs
-router.get('/logs',
-  loggerController.getLogs,
-  (req, res) => res.status(200).json(res.locals.logs));
-
-// route to post all types of logs: client, sever, requests
-router.post('/logs/:type',
-  loggerController.checkLogFile,
-  loggerController.addLogs,
-  loggerController.getLogs,
-  webSocketMiddleware,
-  (req, res) => res.status(200).json(`Added ${req.params}logs`));
-
 // route to delete logs
 router.delete('/logs/delete',
   loggerController.deleteLogs,
