0f1f7772c21b44ced980b87804fbe96d12775402
aye_work1 <amoe.aye@gmail.com>
2021-03-31 19:35:49 -0700

    changes to helper.js

--- server/helpers/helpers.js
+++ server/helpers/helpers.js
@@ -1,7 +1,25 @@
 const fs = require("fs");
 const path = require("path");
 
+// set the current file limit to split
+const FILE_LIMIT = 500000;
+const STARTING_NUM = 100;
+
 const helpers = {};
+
+const getHeadFileNum = async () => {
+  const num = await fs.readFileSync(path.resolve(__dirname, "../data/headFileNum.json"), "utf8");
+  return num;
+};
+
+const updateHeadFileNum = async (num) => {
+  fs.writeFileSync(path.resolve(__dirname, '../data/headFileNum.json'), JSON.parse(num), 'utf8');
+};
+
+const incrementFileNum = async () => {
+  const currentFileNum = await getHeadFileNum();
+  updateHeadFileNum(currentFileNum + 1);
+};
 
 helpers.getAllLogs = async () => {
   const logs = await JSON.parse(
@@ -65,6 +83,51 @@ helpers.checkLogFile = async () => {
     // if there's no error
     return "log file exist";
   });
+};
+
+// middleware to get size of the allLogs.json file
+helpers.getFileSize = async () => {
+  // get the current size of the file and save in res.locals.stats
+  const fileSize = await fs.statSync(path.resolve(__dirname, '../data/allLogs.json'));
+  return fileSize;
+};
+// middlware to rename allLogs.json file to allLogs{fileTracker} format
+helpers.splitFile = async (size) => {
+  let fileFlag;
+  const currentFileNum = await getHeadFileNum();
+  // if the current file size is bigger than fileLimit value
+  if (size > FILE_LIMIT) {
+    // set the res.locals.fileFlag to true
+    fileFlag = true;
+    // rename the current allLogs.json file to allLogs{fileTracker} format
+    fs.rename(path.resolve(__dirname, '../data/allLogs.json'), path.resolve(__dirname, `../data/allLogs${currentFileNum}.json`), () => {
+      console.log('new file created');
+    });
+  }
+  return fileFlag;
+};
+// middleware to reset the value of allLogs.json to empty array
+helpers.resetFile = async (fileFlag) => {
+  // if the res.locals.fileFlag value is true
+  if (fileFlag) {
+    // overwirte the value of allLogs.json file to empty array
+    await fs.writeFileSync(path.resolve(__dirname, '../data/allLogs.json'), JSON.stringify([]), 'utf8');
+    // increment the fileTracker by one
+    incrementFileNum();
+  }
+};
+
+// middleware to delete all old logs
+helpers.deleteOldLogs = () => {
+  const headFile = getHeadFileNum();
+  for (let i = STARTING_NUM; i < headFile + 1; i++) {
+    try {
+      fs.unlinkSync(path.resolve(__dirname, `../data/allLogs${i}.json`));
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  updateHeadFileNum(STARTING_NUM);
 };
 
 module.exports = helpers;
