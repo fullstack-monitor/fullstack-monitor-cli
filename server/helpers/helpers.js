85bc68458c0f294df8c661080760be9aa10b9d34
aye_work1 <amoe.aye@gmail.com>
2021-03-31 20:22:10 -0700

    integrate file split funciton

--- server/helpers/helpers.js
+++ server/helpers/helpers.js
@@ -2,7 +2,8 @@ const fs = require("fs");
 const path = require("path");
 
 // set the current file limit to split
-const FILE_LIMIT = 500000;
+// 1000 for 1k byte
+const FILE_LIMIT = 10000000; // 10MB
 const STARTING_NUM = 100;
 
 const helpers = {};
@@ -18,7 +19,45 @@ const updateHeadFileNum = async (num) => {
 
 const incrementFileNum = async () => {
   const currentFileNum = await getHeadFileNum();
-  updateHeadFileNum(currentFileNum + 1);
+  updateHeadFileNum(parseInt(currentFileNum, 10) + 1);
+};
+
+helpers.getFileSize = async () => {
+  // get the current size of the file and save in res.locals.stats
+  const fileStats = await fs.statSync(path.resolve(__dirname, '../data/allLogs.json'));
+  return fileStats.size;
+};
+// middlware to rename allLogs.json file to allLogs{fileTracker} format
+helpers.splitFile = async (size) => {
+  let fileSplit = false;
+  const currentFileNum = await getHeadFileNum();
+  // if the current file size is bigger than fileLimit value
+  if (size > FILE_LIMIT) {
+    // set the res.locals.fileSplit to true
+    fileSplit = true;
+    // rename the current allLogs.json file to allLogs{fileTracker} format
+    await fs.rename(path.resolve(__dirname, '../data/allLogs.json'), path.resolve(__dirname, `../data/allLogs${currentFileNum}.json`), () => {
+      console.log('new file created');
+    });
+
+    await fs.writeFileSync(path.resolve(__dirname, '../data/allLogs.json'), JSON.stringify([]), 'utf8');
+    // increment the fileTracker by one
+    incrementFileNum();
+  }
+  return fileSplit;
+};
+
+// middleware to delete all old logs
+helpers.deleteOldLogs = async () => {
+  const headFile = await getHeadFileNum();
+  for (let i = STARTING_NUM; i < headFile; i++) {
+    try {
+      fs.unlinkSync(path.resolve(__dirname, `../data/allLogs${i}.json`));
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  updateHeadFileNum(STARTING_NUM);
 };
 
 helpers.getAllLogs = async () => {
@@ -30,6 +69,12 @@ helpers.getAllLogs = async () => {
 };
 
 helpers.storeLogs = async (logs) => {
+  // get the current size of allLogs.json file
+  const fileSize = await helpers.getFileSize();
+
+  // split the log file and return the fileFlag value
+  await helpers.splitFile(fileSize);
+
   let data;
 
   try {
@@ -60,6 +105,7 @@ helpers.storeLogs = async (logs) => {
 };
 
 helpers.deleteLogs = async () => {
+  helpers.deleteOldLogs();
   const res = await fs.writeFileSync(
     path.resolve(__dirname, "../data/allLogs.json"),
     JSON.stringify([]),
@@ -86,48 +132,5 @@ helpers.checkLogFile = async () => {
 };
 
 // middleware to get size of the allLogs.json file
-helpers.getFileSize = async () => {
-  // get the current size of the file and save in res.locals.stats
-  const fileSize = await fs.statSync(path.resolve(__dirname, '../data/allLogs.json'));
-  return fileSize;
-};
-// middlware to rename allLogs.json file to allLogs{fileTracker} format
-helpers.splitFile = async (size) => {
-  let fileFlag;
-  const currentFileNum = await getHeadFileNum();
-  // if the current file size is bigger than fileLimit value
-  if (size > FILE_LIMIT) {
-    // set the res.locals.fileFlag to true
-    fileFlag = true;
-    // rename the current allLogs.json file to allLogs{fileTracker} format
-    fs.rename(path.resolve(__dirname, '../data/allLogs.json'), path.resolve(__dirname, `../data/allLogs${currentFileNum}.json`), () => {
-      console.log('new file created');
-    });
-  }
-  return fileFlag;
-};
-// middleware to reset the value of allLogs.json to empty array
-helpers.resetFile = async (fileFlag) => {
-  // if the res.locals.fileFlag value is true
-  if (fileFlag) {
-    // overwirte the value of allLogs.json file to empty array
-    await fs.writeFileSync(path.resolve(__dirname, '../data/allLogs.json'), JSON.stringify([]), 'utf8');
-    // increment the fileTracker by one
-    incrementFileNum();
-  }
-};
-
-// middleware to delete all old logs
-helpers.deleteOldLogs = () => {
-  const headFile = getHeadFileNum();
-  for (let i = STARTING_NUM; i < headFile + 1; i++) {
-    try {
-      fs.unlinkSync(path.resolve(__dirname, `../data/allLogs${i}.json`));
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  updateHeadFileNum(STARTING_NUM);
-};
 
 module.exports = helpers;
