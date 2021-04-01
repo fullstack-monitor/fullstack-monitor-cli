39aa0b0d837780c1e1d9fec3538ce78626d5d43d
tommyrharper <thomasrobertharper@gmail.com>
2021-04-01 23:32:14 +0100

    Slice response data

--- src/components/LogTable/Response.js
+++ src/components/LogTable/Response.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { Tr, Td } from "@chakra-ui/react";
 
+const MAX_RESPONSE_DATA_LENGTH = 50;
+
 export default function Request({ response, splitView, showMoreLogInfo }) {
+  let { responseData } = response;
+  if (responseData) {
+    responseData = responseData.length > 50
+      ? `${response.responseData.slice(0, MAX_RESPONSE_DATA_LENGTH)}...`
+      : responseData;
+  } else {
+    responseData = "No data.";
+  }
   return (
     <Tr onClick={splitView}>
       <Td>{response.timestamp}</Td>
       <Td>{response.class}</Td>
       <Td>{response.responseStatus}</Td>
-      <Td>{response.responseData ? response.responseData : "no data"}</Td>
+      <Td>{responseData}</Td>
     </Tr>
   );
 }
