ce9914a904ed0cf1e79254a68074e8b3b7c85b8d
tommyrharper <thomasrobertharper@gmail.com>
2021-04-01 23:07:52 +0100

    Refactor LogTable.js

--- src/components/LogTable/LogTable.js
+++ src/components/LogTable/LogTable.js
@@ -13,71 +13,59 @@ import Response from "./Response";
 
 export default function LogTable({ showMoreLogInfo, splitView, logs }) {
   return (
-    <div className="tableContainer">
-      <div className="mainTableContainer">
-        <Table variant="simple">
-          <Thead>
-            <Tr>
-              <Th>TimeStamp</Th>
-              <Th>Type</Th>
-              <Th>Class / Method / Status</Th>
-              <Th>Log / Endpoint / Response Data</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {logs.map((log, i) => {
-              switch (log.class) {
-                case "client":
-                  return (
-                    <Log
-                      log={log}
-                      key={`${log.class}${log.type}${log.timestamp}${log.log}`}
-                      splitView={() => splitView(i)}
-                      showMoreLogInfo={showMoreLogInfo}
-                    />
-                  );
-                case "server":
-                  return (
-                    <Log
-                      log={log}
-                      key={`${log.class}${log.type}${log.timestamp}${log.log}`}
-                      splitView={() => splitView(i)}
-                      showMoreLogInfo={showMoreLogInfo}
-                    />
-                  );
-                case "request":
-                  return (
-                    <Request
-                      request={log}
-                      key={`${log.class}${log.method}${log.timestamp}${log.originalUri}`}
-                      splitView={() => splitView(i)}
-                      showMoreLogInfo={showMoreLogInfo}
-                    />
-                  );
-                case "response":
-                  return (
-                    <Response
-                      response={log}
-                      key={`${log.class}${log.responseStatus}${log.timestamp}`}
-                      splitView={() => splitView(i)}
-                      showMoreLogInfo={showMoreLogInfo}
-                    />
-                  );
-                default:
-                  return <noscript />;
-              }
-            })}
-          </Tbody>
-          <Tfoot>
-            <Tr>
-              <Th>Type</Th>
-              <Th>TimeStamp</Th>
-              <Th>Class</Th>
-              <Th>Log</Th>
-            </Tr>
-          </Tfoot>
-        </Table>
-      </div>
-    </div>
+    <Table variant="simple">
+      <Thead>
+        <Tr>
+          <Th>TimeStamp</Th>
+          <Th>Type</Th>
+          <Th>Class / Method / Status</Th>
+          <Th>Log / Endpoint / Response Data</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {logs.map((log, i) => {
+          switch (log.class) {
+            case "client":
+            case "server":
+              return (
+                <Log
+                  log={log}
+                  key={`${log.class}${log.type}${log.timestamp}${log.log}`}
+                  splitView={() => splitView(i)}
+                  showMoreLogInfo={showMoreLogInfo}
+                />
+              );
+            case "request":
+              return (
+                <Request
+                  request={log}
+                  key={`${log.class}${log.method}${log.timestamp}${log.originalUri}`}
+                  splitView={() => splitView(i)}
+                  showMoreLogInfo={showMoreLogInfo}
+                />
+              );
+            case "response":
+              return (
+                <Response
+                  response={log}
+                  key={`${log.class}${log.responseStatus}${log.timestamp}`}
+                  splitView={() => splitView(i)}
+                  showMoreLogInfo={showMoreLogInfo}
+                />
+              );
+            default:
+              return <noscript />;
+          }
+        })}
+      </Tbody>
+      <Tfoot>
+        <Tr>
+          <Th>Type</Th>
+          <Th>TimeStamp</Th>
+          <Th>Class</Th>
+          <Th>Log</Th>
+        </Tr>
+      </Tfoot>
+    </Table>
   );
 }
