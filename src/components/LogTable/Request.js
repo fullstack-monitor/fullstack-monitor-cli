9a426cdaa999932df6ee887ea52b07fd140f06f8
tommyrharper <thomasrobertharper@gmail.com>
2021-04-03 17:28:48 +0100

    Add icons

--- src/components/LogTable/Request.js
+++ src/components/LogTable/Request.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Tr, Td } from "@chakra-ui/react";
+import { ArrowForwardIcon } from '@chakra-ui/icons';
 import { sanitizeAndShortenRequestResponseData, capitalizeFirstLetter } from "../../helpers/helpers";
 
 export default function Request({ request, splitView, styleObj }) {
@@ -9,7 +10,10 @@ export default function Request({ request, splitView, styleObj }) {
   return (
     <Tr onClick={splitView} style={styleObj}>
       <Td>{timestamp}</Td>
-      <Td>{`${method} ${capitalizeFirstLetter(classType)}`}</Td>
+      <Td>
+        <ArrowForwardIcon color="green.300" marginBottom="2px" />
+        {` ${method} ${capitalizeFirstLetter(classType)}`}
+      </Td>
       <Td>{originalUri}</Td>
       <Td>{sanitizeAndShortenRequestResponseData(requestData)}</Td>
     </Tr>
